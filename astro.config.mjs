1412feaab0f5a37d483fb74bf4590fbba41ac3fb
GonzaloGRomero <108243848+GonzaloGRomero@users.noreply.github.com>
2026-05-25 15:43:50 -0300

    Set base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   output: 'static',
   site: 'https://gonzalogromero.github.io', // Cambiá esto con tu usuario de GitHub
+  base: '/Hub', 
 });
