edc33354f1534e47d6b10c27005ecba9a5ce4a73
GonzaloGRomero <108243848+GonzaloGRomero@users.noreply.github.com>
2026-05-25 15:38:54 -0300

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://YOUR_USER.github.io', // Cambiá esto con tu usuario de GitHub
+  site: 'https://gonzalogromero.github.io', // Cambiá esto con tu usuario de GitHub
 });
